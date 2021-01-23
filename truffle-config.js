97b2de055e86debf0216ecc77a43a1bb865f5185
古田朋宏 <tomohiro@MacBook-Pro.local>
2021-01-23 14:48:07 -0500

    fix deploy setting

--- truffle-config.js
+++ truffle-config.js
@@ -63,8 +63,8 @@ module.exports = {
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
-    kovan: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`),
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${process.env.INFURA_ID}`),
       network_id: 42, // Ropsten's id
       // gas: 5500000, // Ropsten has a lower block limit than mainnet
       // confirmations: 2, // # of confs to wait between deployments. (default: 0)
