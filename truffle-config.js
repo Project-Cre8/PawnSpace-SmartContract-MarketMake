0c5f81136a3993c703ff68ed62ae39dce23d8d29
古田朋宏 <tomohiro@MacBook-Pro.local>
2021-01-23 14:47:32 -0500

    add deploy setting to Kovan

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const mnemonic = process.env.MNEMONIC
 
 module.exports = {
   /**
@@ -66,6 +63,14 @@ module.exports = {
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`),
+      network_id: 42, // Ropsten's id
+      // gas: 5500000, // Ropsten has a lower block limit than mainnet
+      // confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
